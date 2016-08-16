ee956066f5096b0bda283b7f81fab73194cfa82d
Atlante <atlanteavila@gmail.com>
2016-08-15 22:30:32 -0700

    Added message routes and have them working in the back end. We must add the working code in the front end to display and submit messages.

--- public/js/controllers/MessagesController.js
+++ public/js/controllers/MessagesController.js
@@ -2,19 +2,18 @@
 angular.module('MessagesController', ['OwnerService'])
 //add and label the controller
 .controller('MsgCtrl', function($scope, $http, Owner) {
-  $scope.id = '57aa4d5e4b31c711003c7621'
+  $scope.id = '57aa4d5e4b31c711003c7621';
+  $scope.fromId = '57b13554a6b7c011000acac2';
   // empty object will contain user data
-  $scope.msg = {messages: [{
-    from: "xyz",
-    to: "kmn",
+  $scope.msg = {
+    from: $scope.fromId,
+    to: $scope.id,
     time:  Date.now ,
-    msg: "changed "
-    
-  }]
+    msg: "I think puppy love is super awesome!"
 };
 
   $scope.submitMsg = function(){
-    Owner.update( $scope.msg, $scope.id)
+    Owner.addComment( $scope.msg, $scope.id, $scope.fromId)
     
     .success(function(){
       console.log('Message Added!')
